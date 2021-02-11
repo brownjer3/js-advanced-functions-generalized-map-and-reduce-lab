221ece41bebcc130fe419130fb37ec36b081b0f6
jerry <brownjer3@gmail.com>
2021-02-10 16:32:09 -0800

    commit tree 687b66754daadb380f50485ea63ab7343be8fa71

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your functions here
+function map(array, fn) {
+    let newArr = []
+    array.forEach(element => {
+        newArr.push(fn(element))
+    })
+    return newArr
+}
+
+function reduce(array, fn, start) {
+    let total, arr;
+    if (!!start) {
+        total = start
+        arr = [...array]
+    } else {
+        total = array[0]
+        arr = array.slice(1)
+    }
+    
+    arr.forEach(element => {
+        total = fn(element, total)
+    })
+
+    return total
+}
+
+// function reduceToTotal(array, start=0) {
+//     return array.reduce(((total, amount) => total+amount), start)
+// }
